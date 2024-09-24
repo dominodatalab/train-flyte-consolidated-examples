20a9fa16b749835c2e07f342085dcb2733204f77
ddl-rliu <richard.liu@dominodatalab.com>
2024-09-24 15:16:32 -0700

    Add common file types

--- data/prep-data.py
+++ data/prep-data.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 
 try:
     named_output = "model"
@@ -10,5 +11,13 @@ for named_output in ["processed_data"] + [f"processed_data{i}" for i in range(2,
     try:
         with open("/workflow/outputs/{}".format(named_output), "w") as f:
             f.write("a,b,c\n1,2,3")
+    except:
+        pass
+
+for ext in ["csv", "docx", "html", "pdf", "rtf", "sas7bdat", "xlsx"]:
+    try:
+        with open("/workflow/outputs/data{}".format(ext), "wb") as f:
+            with open("/mnt/train-flyte-consolidated-examples/data/data.{}".format(ext), "rb") as src:
+                shutil.copyfileobj(src, f)
     except:
         pass
