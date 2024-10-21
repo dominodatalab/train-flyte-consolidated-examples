17edb627755ad271ae7159d7ea2cbf5e1cf96292
ddl-rliu <richard.liu@dominodatalab.com>
2024-10-21 10:43:57 -0700

    x

--- README.md
+++ README.md
@@ -18,7 +18,8 @@ git clone https://github.com/dominodatalab/train-flyte-consolidated-examples.git
 ## Workflows Overview
 
 ```bash
-invalid_artifacts_workflow.py
+invalid_wf_level_artifacts_workflow.py
+invalid_task_level_artifacts_workflow.py
 
 artifacts_workflow.py
 caching_workflow.py
--- bin/run-one.sh
+++ bin/run-one.sh
@@ -3,6 +3,7 @@ echo "" > $TFCE_LOG
 
 TFCE_WORKFLOWS="./train-flyte-consolidated-examples/workflows"
 TFCE_RUN="pyflyte run --remote"
-$TFCE_RUN $TFCE_WORKFLOWS/invalid_artifacts_workflow.py wf &>> $TFCE_LOG
+$TFCE_RUN $TFCE_WORKFLOWS/invalid_wf_level_artifacts_workflow.py wf &>> $TFCE_LOG
+$TFCE_RUN $TFCE_WORKFLOWS/invalid_task_level_artifacts_workflow.py wf &>> $TFCE_LOG
 # $TFCE_RUN $TFCE_WORKFLOWS/artifacts_workflow.py wf &>> $TFCE_LOG
 # $TFCE_RUN $TFCE_WORKFLOWS/old_artifacts_workflow.py wf &>> $TFCE_LOG
